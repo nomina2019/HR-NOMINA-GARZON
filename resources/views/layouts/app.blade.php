9f36ad2554cd521ddb134a3a73fd4c6edb5977da
nomina2019 <lsaracual@elgarzon.com>
2019-08-28 15:21:59 -0400

    hi

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Styles -->
-    <link rel="stylesheet" href="https://bootswatch.com/4/flatly/bootstrap.css">
+      <link rel="stylesheet" href="../4/flatly/bootstrap.css" media="screen">
 
   <!-- Scripts -->
   <script>
