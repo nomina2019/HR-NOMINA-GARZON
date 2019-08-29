c76da747f766958eb05571027bea7caf45452c04
nomina2019 <lsaracual@elgarzon.com>
2019-08-29 09:35:32 -0400

    ..

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Styles -->
-    <link rel="stylesheet" href="/css/bootstrap.css">
+    <link  href="/css/app.css" rel="stylesheet">
 
 
 </head>
